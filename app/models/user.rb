00fffec027ee09f3753e9fc1e6849f62c12b8244
KazTateiwa <kaz.t@verizon.net>
2017-03-28 15:56:39 -0700

    Sign up (#4)
    * user sign up w/ email pass, name and avatar
    
    * added the test for the sign-up feature

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # reference to activities
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  has_many :activities
+  # provides support for paperclip for the user avatar.
+  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+  # This validation requres that Users attach an avatar photo upon creation.
+  validates_attachment :image, presence: true,
+    content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
+    size: { in: 0..10.megabytes }
+end
