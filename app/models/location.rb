ec80cba8943ffea3496f275d340b38f71fd0095f
Benjino <siegfriedmedia.com>
2017-03-31 13:25:44 -0700

    Completed testing for activity-layout, added strftime to next_at

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,6 @@
 class Location < ApplicationRecord
   has_many :activities
+  geocoded_by :address
+  after_validation :geocode
+
 end
--- spec/features/activity_layouts_spec.rb
+++ spec/features/activity_layouts_spec.rb
@@ -3,6 +3,23 @@ require 'rails_helper'
 RSpec.feature "ActivityLayouts", type: :feature do
   context "Activies > Show Layout" do
     Steps "Layout of the Activities > Show page" do
+      Given "I have an activity" do
+        user = User.create!(email: 'test@example.com', password: 'testing123')
+        location = Location.create!( "name"=>"Sunset Park",
+                                    "address"=>"555 Sunset Blvd",
+                                    "city"=>"Sunset",
+                                    "state"=>"CA",
+                                    "zip"=>"92104",
+                                    "latitude"=>123.23,
+                                    "longitude"=>31.22)
+        Activity.create!(name: "Jazercize",
+                        user: user,
+                        website: 'http://google.com',
+                        description: "This is the song that never ends, yes it goes on and on my friends, some people, started singining it not knowing what it was, but they'll just keeep on singing it forever just because this is the song that never ends, yes it goes on and on my friends, some people, started singining it not knowing what it was, but they'll just keeep on singing it forever",
+                        next_at: 1.day.from_now,
+                        schedule: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        location: location)
+      end
       Given 'I click on the sign up link on the homepage and am brought to the sign up page' do
         visit '/users/sign_up'
       end
@@ -15,34 +32,33 @@ RSpec.feature "ActivityLayouts", type: :feature do
         fill_in 'Password confirmation', with: 'password'
         click_button 'Sign up'
       end
-
-      Given "I visit localhost:3000/users/sign_in" do
-        visit "Log in"
-        expect(page).to have_content("Log in")
+      Then "I see a successful sign-in alert" do
+        expect(page).to have_content("You have signed up successfully")
       end
-      Then "I visit localhost:3000/activities" do
+      Given "I visit localhost:3000/activities" do
         visit "/activities"
       end
-      Then "I can fill email and password" do
-        fill_in 'Email', with: 'bsems@me.com'
-        fill_in 'Password', with: 'password'
-        click_link 'Sign up'
-      end
-      Then "I am taken to localhost:3000" do
-        expect(page).to have_content("Signed in successfully")
-      end
       Then "I see page title" do
-        expect(page).to have_content("Welcome to FitFinder!")
+        expect(page).to have_content("Welcome to FitFinder")
       end
-      Then "I click the Show text link" do
+      When "I click the Show text link" do
         click_link 'Show'
       end
-      Then "I visit localhost:3000/activities/2" do
-        visit '/activities/2'
+      Then "I can see content for the created Activity" do
+        expect(page).to have_content("555 Sunset Blvd")
+        expect(page).to have_content("Sunset")
+        expect(page).to have_content("CA")
+        expect(page).to have_content("Jazercize")
+        expect(page).to have_content("http://google.com")
       end
-      Then "I can see the layout of the activity show page" do
 
-      end
+      # capybara debugging command --> save_and_open_page
+      # Then "I visit localhost:3000/activities/2" do
+      #   visit '/activities/2'
+      # end
+      # Then "I can see the layout of the activity show page" do
+
+      # end
     end
   end
 end
