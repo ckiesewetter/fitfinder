b84009fd8dc4efb8c8065564b97f0aff68221d2c
kaztateiwa <kaz.t@verizon.net>
2017-03-31 10:21:08 -0700

    location saving geocoding

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,6 @@
 class Location < ApplicationRecord
+  geocoded_by :address
+  # auto-fetch coordinates and the condition is for preventing fetching the same address more than once
+  after_validation :geocode
   has_many :activities
 end
