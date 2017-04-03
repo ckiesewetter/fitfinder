3984dbe42af6213d1152bdc42a58b555c27ecbf0
acunit <f.andrew.carroll@gmail.com>
2017-04-03 12:02:59 -0700

    Log out feature setup and tested, fixed some other tests as well.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,13 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address                =>  'smtp.sendgrid.net',
-  :port                   =>  '587',
-  :authentication         =>  :plain,
-  :user_name              =>  'app65740160@heroku.com',
-  :password               =>  'wktds0oc2454',
-  :domain                 =>  'heroku.com',
-  :enable_starttls_auto   =>  true
-}
+# Was global now will run on environments > development and production only, excluding test.
+unless Rails.env.test?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address                =>  'smtp.sendgrid.net',
+    :port                   =>  '587',
+    :authentication         =>  :plain,
+    :user_name              =>  'app65740160@heroku.com',
+    :password               =>  'wktds0oc2454',
+    :domain                 =>  'heroku.com',
+    :enable_starttls_auto   =>  true
+  }
+end
