cb1c518e80cb11cb9ba1cb516f86819c2829ce32
Andrew Carroll <f.andrew.carroll@gmail.com>
2017-04-10 14:48:35 -0700

    Participant list added and tested (#26)

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,7 @@
 class Activity < ApplicationRecord
   belongs_to :location, optional: true
   belongs_to :user, optional: true
+  has_many :participation
 
   def organizer_name
     if self.user.present?
