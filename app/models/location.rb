53a4dc5457977edb887ae00cb34e1d9868870ad6
Andrew Carroll <f.andrew.carroll@gmail.com>
2017-04-11 11:55:20 -0700

    added image to location and worked on some styling for this image (#33)

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,6 +4,12 @@ class Location < ApplicationRecord
   after_validation :geocode
   has_many :activities
 
+  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+
+  validates_attachment :image,
+    content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
+    size: { in: 0..10.megabytes }
+
   def full_address
     "#{address}, #{city}, #{state}, #{zip}"
   end
